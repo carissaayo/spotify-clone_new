3c0c184db9f1ab91549a1dc1bf4ec5fe379c4af6
Yussuf <you@example.com>
2023-09-30 21:41:54 +0100

    fixed a little useeffect dependency warning

--- components/AuthModal.tsx
+++ components/AuthModal.tsx
@@ -24,7 +24,7 @@ const AuthModal = () => {
       router.refresh();
       onClose();
     }
-  }, [session]);
+  }, [session, router, onClose]);
 
   const onChange = (open: boolean) => {
     if (!open) {
